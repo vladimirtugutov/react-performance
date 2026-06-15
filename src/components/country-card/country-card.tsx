e49600d3c8b437351674ef40a3fd2566f62b52b7
vladimirtugutov <vladimir.tugutov@gmail.com>
2026-06-15 19:15:28 -0300

    feat: wrap CountryCard with React.memo and memoize yearDataMap

--- src/components/country-card/country-card.tsx
+++ src/components/country-card/country-card.tsx
@@ -1,3 +1,4 @@
+import { memo, useMemo } from 'react';
 import type { Country } from '../../types';
 import { DataTable } from '../data-table/data-table';
 import {
@@ -15,8 +16,8 @@ type CountryCardProps = {
   selectedColumns: string[];
 };
 
-export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryCardProps) => {
-  const yearDataMap = createYearDataMap(country.data);
+export const CountryCard = memo(({ country, selectedYear, selectedColumns }: CountryCardProps) => {
+  const yearDataMap = useMemo(() => createYearDataMap(country.data), [country.data]);
   const population = getPopulationForYear(yearDataMap, selectedYear);
   const co2 = getCo2ForYear(yearDataMap, selectedYear);
 
@@ -39,4 +40,6 @@ export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryC
       <DataTable data={country.data} year={selectedYear} columns={selectedColumns} />
     </div>
   );
-};
+});
+
+CountryCard.displayName = 'CountryCard';
